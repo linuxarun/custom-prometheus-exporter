41f7f6aee9236650d9caae7ee6bc9b0d4b8108b8
linuxarun <arun.cafe@gmail.com>
2022-06-11 15:58:35 +0530

    Update prometheus.py

--- prometheus.py
+++ prometheus.py
@@ -1,15 +1,40 @@
 import os,stat
 from time import sleep
+from metric_buckets import buckets
+from collections import OrderedDict
 
 service = service = os.uname()[1]
 
+# Example:
+# metrics_data_map = {
+#     http_requests_ms_total : {
+#         "uri=jsonrpc-intern/insertrentalcontractv2.p;channel=website;" = [count, time_taken]
+#     }
+# }
 metrics_data_map = {}
 
+
+# Example:
+# buckets_data_map = {
+#      http_requests_ms_total : {
+#          website : {
+#                 insertrentalcontractv2 : {
+#                       "250": 0,
+#                       "500": 2        
+#                 }
+#             }
+#      }
+#  }
+buckets_data_map = {}
+
 metrics_file_path = "/root/prometheus/data/"
+#metrics_file_path = "/tmp/"
 metrics_file = metrics_file_path + "metrics_data"
 
 log_file_path = "/appl/log/metrics/"
+#log_file_path = "/Users/oyo/python-server/"
 log_file = log_file_path + "application_metrics.log"
+#log_file = log_file_path + "server.log"
 
 def main():
     gather_metrics()
@@ -19,62 +44,113 @@ def prometheus():
     data = f.read()
     return data
 
-def areEqual(arr1, arr2, n, m):
-    if (n != m):
-        return False
 
-    arr1.sort()
-    arr2.sort()
-    for i in range(0, n):
-        if (arr1[i] != arr2[i]):
-            return False
-    return True
+def get_label_format(label, value):
+    if label is None or value is None:
+        return ""
+    return label + "=" + "\"" + value + "\""
 
 
-def create_metrics_data(metric, keys, time_taken):   
-    if metric not in  metrics_data_map:
-        metrics_data_map[metric] = []
-        metrics_data_map[metric].append([keys, 1.0, time_taken])
+def is_bucketing_allowed(metric, channel, uri):
+    return metric in buckets and channel in buckets[metric] and uri in buckets[metric][channel] and len(buckets[metric][channel][uri]) != 0
+
+def create_buckets_data_with_fallback(metric, channel, uri):
+    if metric not in buckets_data_map:
+        buckets_data_map[metric] = {}
+
+    if channel not in buckets_data_map[metric]:
+        buckets_data_map[metric][channel] = {}
+
+    if uri not in buckets_data_map[metric][channel]:
+        buckets_data_map[metric][channel][uri] = OrderedDict()
+
+    rebalance_required = False
+    for le in buckets[metric][channel][uri]:
+        if le not in buckets_data_map[metric][channel][uri]:
+            rebalance_required = True
+            break
+
+    if rebalance_required:
+        buckets_data_map[metric][channel][uri] = OrderedDict()
+        for le in buckets[metric][channel][uri]:
+            buckets_data_map[metric][channel][uri][le] = 0.0
+
+
+def create_buckets_data(metric, channel, uri, time_taken):
+    if metric is None or channel is None or uri is None or time_taken is None:
         return
 
-    found = False
-    for item in metrics_data_map[metric]:
-        if areEqual(item[0], keys, len(item[0]), len(keys)) == True:
-            item[1] += 1
-            if time_taken is not None:
-                if item[2] is None:
-                    item[2] = 0.0
-                item[2] += time_taken
-            found = True
-            break
-    if found == False:
-        metrics_data_map[metric].append([keys, 1.0, time_taken])
+    if not is_bucketing_allowed(metric, channel, uri):
+        return
+    
+    create_buckets_data_with_fallback(metric, channel, uri)
+
+    if time_taken == 0.0:
+        return
+
+    for le in buckets[metric][channel][uri]:
+        if time_taken < float(le):
+            buckets_data_map[metric][channel][uri][le] += 1
+
+
+def create_metrics_data(metric, keys_data, time_taken):   
+    if metric not in  metrics_data_map:
+        metrics_data_map[metric] = {}
+
+    metric_data = metrics_data_map[metric]
+    if keys_data not in metric_data:
+        metrics_data_map[metric][keys_data] = [1.0, time_taken]
+        return
+
+    metrics_data_map[metric][keys_data][0] += 1
+    if time_taken is not None:
+        if metrics_data_map[metric][keys_data][1] is None:
+            metrics_data_map[metric][keys_data][1] = 0.0
+        metrics_data_map[metric][keys_data][1] += time_taken
+
+
+
+def buckets_data_string(metrics_str):
+    if metrics_str is None:
+        metrics_str = ""
+    for metric, metric_data in buckets_data_map.items():
+        for channel, channel_data in metric_data.items():
+            for uri, uri_data in channel_data.items():
+                for le, count in uri_data.items():
+                    metrics_str += metric + "_bucket" + "{" + get_label_format("app_service", service) + "," + get_label_format("channel", channel) + "," + get_label_format("uri", uri) + "," + get_label_format("le", le) + "," + "} " + str(count) + "\n"
+    return metrics_str
 
 
 def write_metrics():
     metric_str = ""
-    for metric,data in metrics_data_map.items():
-        if data is None or len(data) == 0:
+    for metric,overall_metric_data in metrics_data_map.items():
+        if overall_metric_data is None or not bool(overall_metric_data):
             continue
-        for data_value in data:
-            if data_value is None or len(data_value[0]) == 0:
-                continue
-            metric_str += metric + "_count" + "{" + "app_service=" + "\"" + service + "\"" + ","
-            for item in data_value[0]:
-                metric_str += item.split('=')[0] + "=" + "\"" + item.split('=')[1] + "\"" + ","
-            metric_str += "} " + str(data_value[1]) + "\n"
 
-            if data_value[2] is not None:
+        for metric_data,data_value in overall_metric_data.items():
+            if data_value is None or len(data_value) == 0:
+                continue
+            labels_list = metric_data.split(";")
+            metric_str += metric + "_count" + "{" + get_label_format("app_service", service) + ","
+            for item in labels_list:
+                item_list = item.split('=')
+                metric_str += get_label_format(item_list[0], item_list[1]) + ","
+            metric_str += "} " + str(data_value[0]) + "\n"
+
+            if data_value[1] is not None:
                 metric_str += metric + "_sum" + "{" + "app_service=" + "\"" + service + "\"" + ","
-                for item in data_value[0]:
-                    metric_str += item.split('=')[0] + "=" + "\"" + item.split('=')[1] + "\"" + ","
-                metric_str += "} " + str(data_value[2]) + "\n"
+                for item in labels_list:
+                    item_list = item.split('=')
+                    metric_str += get_label_format(item_list[0], item_list[1]) + ","
+                metric_str += "} " + str(data_value[1]) + "\n"
     
+    metric_str += buckets_data_string("")
     if not os.path.exists(metrics_file_path):
         os.makedirs(metrics_file_path)
     f = open(metrics_file,"w")
     f.write(metric_str)
     f.close()
+
 
 def gather_metrics():
     for line in tail_log_file():
@@ -100,7 +176,7 @@ def gather_metrics():
             try:
                 time_taken = float(metrics_map.pop('time_taken'))
             except Exception as e:
-                time_taken = None
+                time_taken = None   
 
             labels_data = []
             for key,value in metrics_map.items():
@@ -110,7 +186,9 @@ def gather_metrics():
                     value = "None"
                 labels_data.append(key + "=" + value)
 
-            create_metrics_data(metric, labels_data, time_taken)
+            labels_data.sort()
+            create_metrics_data(metric, ";".join(labels_data), time_taken)
+            create_buckets_data(metric, metrics_map['channel'], metrics_map['uri'], time_taken)
             write_metrics()
 
         except Exception as e:
